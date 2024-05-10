92923dc13464a5cc1ce54c050445de8526fc0795
ElNegis <128728636+ElNegis@users.noreply.github.com>
2024-05-09 21:07:26 -0400

    update
    esta vez esta bien

--- frontend/login/RestablecerContrasenaAltosMandos/nuevo_codigo2.js
+++ frontend/login/RestablecerContrasenaAltosMandos/nuevo_codigo2.js
@@ -3,25 +3,7 @@ document.getElementById("reset-password-btn").addEventListener("click", function
     
     var username = document.getElementById("username").value;
     var email = document.getElementById("email").value;
-
-    // Verificar si el usuario tiene la terminación @trabajador en el correo
-    if (email.endsWith("@trabajador.com")) {
-        // Verificar si el usuario es "camilo" y el correo es "rm144@trabajador.com"
-        if (username === "camilo" && email === "rm144@trabajador.com") {
-            // Mostrar mensaje con el número de contacto para recuperar la contraseña
-            var message = document.getElementById("message");
-            message.textContent = "Para recuperar su contraseña, comuníquese al: 60515432";
-            message.style.color = "green";
-        } else {
-            // Mostrar mensaje de error si las credenciales no son correctas
-            var message = document.getElementById("message");
-            message.textContent = "Usuario o correo electrónico incorrectos.";
-            message.style.color = "red";
-        }
-    } else {
-        // Mostrar mensaje de error si el correo no tiene la terminación correcta
-        var message = document.getElementById("message");
-        message.textContent = "Solo los usuarios con correo '@trabajador.com' pueden recuperar su contraseña por aquí.";
-        message.style.color = "red";
-    }
+    var message = document.getElementById("message");
+        message.textContent = "Para recuperar su contraseña, comuníquese al: 60515432";
+        message.style.color = "green";
 });
