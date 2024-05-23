c9f06490ba64f6648ceab041762ae1f4d87ca27d
Camilo Mendez <144707673+CamiloMendez23@users.noreply.github.com>
2024-05-23 01:59:19 -0400

    Realizadas correcciones en la ventana modal de cuenta creada exitosamente

--- frontend/login/crearCuenta/crear_cuenta.js
+++ frontend/login/crearCuenta/crear_cuenta.js
@@ -12,7 +12,6 @@ document.getElementById("signup-btn").addEventListener("click", function(event) 
         return;
     }
 
-
     // Validar la contraseña
     if (!validatePassword(password)) {
         var message = document.getElementById("message");
@@ -20,22 +19,35 @@ document.getElementById("signup-btn").addEventListener("click", function(event) 
         return;
     }
 
-
     // Guardar el nombre de usuario para evitar duplicados
     localStorage.setItem(username, true);
 
-    // Aquí iría la lógica para crear la cuenta
-    // Por ahora simplemente mostramos un mensaje de éxito
-    var message = document.getElementById("message");
-    message.textContent = "¡Cuenta creada exitosamente!";
-    message.style.color = "green";
+    // Mostrar el modal de éxito
+    showModal();
+    
+    // Cerrar automáticamente el modal después de 4 segundos
+    setTimeout(closeModal, 3000);
 });
 
-// Función para validar la contraseña
 function validatePassword(password) {
     // Requisitos de la contraseña: longitud mínima de 8 caracteres, al menos un número, una letra mayúscula y un carácter especial
     var passwordRegex = /^(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.*[a-z]).{8,}$/;
     return passwordRegex.test(password);
+}
+
+function showModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "flex"; // Cambiar a 'flex' para centrarlo
+}
+
+function closeModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+// Ocultar la ventana modal al cargar la página
+window.onload = function() {
+    closeModal();
 }
 
 // Mostrar u ocultar la contraseña
