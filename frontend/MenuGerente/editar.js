129350d0a4f65e8f126ffa70734c15dc472e1459
Santiago Machicado <santiago.carlos.machicado@gmail.com>
2024-06-03 10:48:35 -0400

    diseño

--- frontend/MenuGerente/editar.js
+++ frontend/MenuGerente/editar.js
@@ -1,19 +1,35 @@
 
-const openModal = document.querySelector(".editarm");
-const modal = document.querySelector(".modal");
-const cerrar = document.querySelector(".cerrar");
+const openModal = document.getElementById("editarm");
+const modal = document.getElementById("edit-modal");
+const closeModal = document.getElementById("close-modal");
 const cancelButton = document.querySelector(".cancel-btn");
 
+const openDeleteModal = document.getElementById("eliminar_m");
+const deleteOverlay = document.getElementById("delete-overlay");
+const closeDeleteDialog = document.getElementById("close-delete-dialog");
+const denyDeleteButton = document.getElementById("deny-delete");
+
 openModal.addEventListener('click', () => {
-    modal.classList.add('modal-show');
+    modal.classList.add('modal--show');
 });
 
-cerrar.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal.classList.remove('modal-show');
+closeModal.addEventListener('click', () => {
+    modal.classList.remove('modal--show');
 });
 
 cancelButton.addEventListener('click', (e) => {
     e.preventDefault();
-    modal.classList.remove('modal-show');
+    modal.classList.remove('modal--show');
+});
+
+openDeleteModal.addEventListener('click', () => {
+    deleteOverlay.classList.add('show');
+});
+
+closeDeleteDialog.addEventListener('click', () => {
+    deleteOverlay.classList.remove('show');
+});
+
+denyDeleteButton.addEventListener('click', () => {
+    deleteOverlay.classList.remove('show');
 });
