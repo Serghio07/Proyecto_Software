9949cea5cd3f56bfa99f564ab45fcfe12259b854
ElNegis <128728636+ElNegis@users.noreply.github.com>
2024-06-03 10:57:30 -0400

    index update

--- test/routes/index.js
+++ test/routes/index.js
@@ -1,7 +1,7 @@
 const { Router} =  require('express');
 const router = Router();
 
-const { getUsers, createUser, getUserById, deleteUser, updateUser, getRegister, getRegisterById, createRegister, deleteRegister, updateRegister} = require('../controllers/index.controller');
+const { getUsers, createUser, getUserById, deleteUser, updateUser, getRegister, getRegisterById, createRegister, deleteRegister, updateRegister, getDetalleOrdenes, getDetalleOredenesById, createDetalleOrdenes, deleteDetalleOrdenes, updateDetalleOrdenes, getOrdenes, getOrdenesById, createOrdenes, deleteOrdenes, updateOrdenes, getComidas, getComidaById, createComida, deleteComida, updateComida} = require('../controllers/index.controller');
 
 router.get('/users',getUsers);
 router.get('/users/:id',getUserById);
@@ -13,5 +13,28 @@ router.get('/regis/:id',getRegisterById);
 router.post('/regis',createRegister);
 router.delete('/regis/:id',deleteRegister);
 router.put('/regis/:id',updateRegister);
+
+
+router.get('/comidas',getComidas);
+router.get('/comidas/:id',getComidaById);
+router.post('/comidas',createComida);
+router.delete('/comidas/:id',deleteComida);
+router.put('/comidas/:id',updateComida);
+router.get('/reservas',getRegister);
+router.get('/reservas/:id',getRegisterById);
+router.post('/reservas',createRegister);
+router.delete('/reservas/:id',deleteRegister);
+router.put('/reservas/:id',updateRegister);
+
+router.get('/oredenes',getOrdenes);
+router.get('/ordenes/:id',getOrdenesById);
+router.post('/ordenes',createOrdenes);
+router.delete('/ordenes/:id',deleteOrdenes);
+router.put('/ordenes/:id',updateOrdenes);
+router.get('/destalle_ordenes',getDetalleOrdenes);
+router.get('/destalle_ordenes/:id',getDetalleOredenesById);
+router.post('/destalle_ordenes',createDetalleOrdenes);
+router.delete('/destalle_ordenes/:id',deleteDetalleOrdenes);
+router.put('/destalle_ordenes/:id',updateDetalleOrdenes);
 
 module.exports = router;
