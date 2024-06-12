44c7ed19c149245cb5183318e6fad8c4ca6a9533
ElNegis <128728636+ElNegis@users.noreply.github.com>
2024-06-12 18:13:47 -0400

    ya esta

--- test/controllers/index.controller.js
+++ test/controllers/index.controller.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL|| 'postgres://postgres:nimda@localhost:5433/restaurante',
+    connectionString: process.env.DATABASE_URL|| 'postgres://postgres:admin@localhost:5433/restaurante',
     ssl: process.env.DATABASE_URL ? true : false
 })
 
