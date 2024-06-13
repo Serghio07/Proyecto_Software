efb40f0a8c1105e9fa81a3405d17c2c236461825
Santiago Machicado <santiago.carlos.machicado@gmail.com>
2024-06-12 22:44:39 -0400

    token

--- frontend/login/RestablecerContrasenaUsuarios/nuevo_codigo.js
+++ frontend/login/RestablecerContrasenaUsuarios/nuevo_codigo.js
@@ -4,10 +4,8 @@ document.getElementById("reset-password-btn").addEventListener("click", function
     var username = document.getElementById("username").value;
     var email = document.getElementById("email").value;
 
-    // Verificar si el usuario tiene la terminación @usuario en el correo
-    if (email.endsWith("@usuario.com")) {
         // Verificar si el usuario es "camilo" y el correo es "rm144@usuario.com"
-        if (username === "camilo" && email === "rm144@usuario.com") {
+        if (true) {
             // Mostrar mensaje con el número de contacto para recuperar la contraseña
             var message = document.getElementById("message");
             message.textContent = "Para recuperar su contraseña, comuníquese al: 60515432";
@@ -18,10 +16,4 @@ document.getElementById("reset-password-btn").addEventListener("click", function
             message.textContent = "Usuario o correo electrónico incorrectos.";
             message.style.color = "red";
         }
-    } else {
-        // Mostrar mensaje de error si el correo no tiene la terminación correcta
-        var message = document.getElementById("message");
-        message.textContent = "Solo los usuarios con correo '@usuario.com' pueden recuperar su contraseña por aquí.";
-        message.style.color = "red";
-    }
 });
